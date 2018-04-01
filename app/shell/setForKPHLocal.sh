76502144de1245f6759dfb687e16043a38f7d134
lnelson <lnelson@parc.com>
2018-03-31 18:03:20 -0700

    Remove old shell routines

--- app/shell/setForKPHLocal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Copy over settings & mobil-config for KPH"
-cp ./shell/settings.kph.local.txt  ./settings.json
-cp ./shell/mobile-config-kph.txt  ./mobile-config.js
-echo "Remember to delete the '.meteor/local' directory"
-echo "change the AndroidManifest after build & rebuild too"
-echo 'change android:windowSoftInputMode="adjustResize"'
-echo ' to '
-echo 'change android:windowSoftInputMode="adjustPan"'
