3529550cb277e6a21785a9a2e63db0903af547c3
lnelson <lnelson@parc.com>
2018-03-31 17:45:54 -0700

    Remove master app structure so it can be replaced with silver app

--- app/shell/setForSilverLocal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Copy over settings & mobil-config for Silver"
-cp ./shell/settings.silver.local.txt  ./settings.json
-cp ./shell/mobile-config-silver.txt  ./mobile-config.js
-echo "Remember to delete the '.meteor/local' directory"
-echo "change the AndroidManifest after build & rebuild too"
-echo 'change android:windowSoftInputMode="adjustResize"'
-echo ' to '
-echo 'change android:windowSoftInputMode="adjustPan"'
